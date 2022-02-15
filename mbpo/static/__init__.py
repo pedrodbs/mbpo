9ebfb6271a7ef621e2c7af51dad7fae1f293e4e2
Pedro Sequeira <pedrodbs@gmail.com>
2022-02-14 22:06:32 -0800

    modified import_fns method to allow running from arbitrary location, instead of mbpo project root dir

--- mbpo/static/__init__.py
+++ mbpo/static/__init__.py
@@ -5,13 +5,14 @@ import pdb
 
 
 def import_fns(path, file, fns_name='StaticFns'):
-	full_path = os.path.join(path, file)
-	import_path = full_path.replace('/', '.')
-	module = importlib.import_module(import_path)
-	fns = getattr(module, fns_name)
-	return fns
+    full_path = os.path.join(path, file)
+    import_path = full_path.replace('/', '.')
+    module = importlib.import_module(import_path)
+    fns = getattr(module, fns_name)
+    return fns
 
-cwd = 'mbpo/static'
+
+cwd = os.path.relpath(os.path.dirname(os.path.realpath(__file__)), os.getcwd())  # 'mbpo/static'
 files = os.listdir(cwd)
 ## remove __init__.py
 files = filter(lambda x: '__' not in x, files)
@@ -19,7 +20,7 @@ files = filter(lambda x: '__' not in x, files)
 files = map(lambda x: x.replace('.py', ''), files)
 
 ## {env: StaticFns, ... }
+cwd = 'mbpo/static'
 static_fns = {file.replace('_', ''): import_fns(cwd, file) for file in files}
 
 sys.modules[__name__] = static_fns
-
